1e9dc8858f1e3725081fe6fac29382a0946b7794
YiChih Wang <andywang0607@gmail.com>
2021-06-18 20:22:03 +0800

    [chore] Implement thread-safe queue
    Note:
    - For queue operation in multi-thread

---
+++ src/datastructure/threadsafe_queue.h
@@ -0,0 +1,76 @@
+#ifndef THREADSAFE_QUEUE_H
+#define THREADSAFE_QUEUE_H
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+class AVPacket;
+class AVFrame;
+template<typename T>
+class threadsafe_queue
+{
+private:
+    mutable std::mutex mut;
+    std::queue<T> data_queue;
+    std::condition_variable data_cond;
+public:
+    threadsafe_queue()
+    {}
+    threadsafe_queue(threadsafe_queue const& other)
+    {
+        std::lock_guard<std::mutex> lk(other.mut);
+        data_queue=other.data_queue;
+    }
+    void push(T new_value)
+    {
+        std::lock_guard<std::mutex> lk(mut);
+        data_queue.push(new_value);
+        data_cond.notify_one();
+    }
+    void wait_and_pop(T& value)
+    {
+        std::unique_lock<std::mutex> lk(mut);
+        data_cond.wait(lk, std::chrono::milliseconds(11), [this]{return !data_queue.empty();});
+        value=data_queue.front();
+        data_queue.pop();
+    }
+    std::shared_ptr<T> wait_and_pop()
+    {
+        std::unique_lock<std::mutex> lk(mut);
+        data_cond.wait_for(lk, std::chrono::milliseconds(11), [this]{return !data_queue.empty();});
+        if(data_queue.empty()) return nullptr;
+        std::shared_ptr<T> res(std::make_shared<T>(data_queue.front()));
+        data_queue.pop();
+        return res;
+    }
+    bool try_pop(T& value)
+    {
+        std::lock_guard<std::mutex> lk(mut);
+        if(data_queue.empty())
+            return false;
+        value=data_queue.front();
+        data_queue.pop();
+        return true;
+    }
+    std::shared_ptr<T> try_pop()
+    {
+        std::lock_guard<std::mutex> lk(mut);
+        if(data_queue.empty())
+            return nullptr;
+        std::shared_ptr<T> res(std::make_shared<T>(data_queue.front()));
+        data_queue.pop();
+        return res;
+    }
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lk(mut);
+        return data_queue.empty();
+    }
+    int size() const
+    {
+        std::lock_guard<std::mutex> lk(mut);
+        return data_queue.size();
+    }
+};
+
+#endif // THREADSAFE_QUEUE_H
